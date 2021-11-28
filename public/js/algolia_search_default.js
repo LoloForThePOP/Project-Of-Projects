4028bf8e44c79562ca668c9487d4a6af721f1b27
LoloForThePOP <57801012+LoloForThePOP@users.noreply.github.com>
2021-11-28 20:07:48 +0100

    homepage ui search experience > md screens

--- public/js/algolia_search_default.js
+++ public/js/algolia_search_default.js
@@ -1,21 +1,26 @@
-  $(document).ready(function(){
-
-    $(".js-close-search-experience").click(function() {
-
-      $(".search-experience-container").hide();
-      $("#main-body-container").show();
-
-    });
-
-    $(".js-reset-search-icon").click(function() {
-
-      $('#navbar_sm_search_input').val('');
-      document.querySelector('#searchbox .ais-SearchBox-input').value = '';
-      search.helper.setQuery(document.querySelector('#searchbox .ais-SearchBox-input').value).search();
-
-    });
+$(document).ready(function(){
 
 
+  //search panel close button on large screens
+  $(".js-close-search-experience").click(function() {
+
+    $(".search-experience-container").hide();
+    $("#main-body-container").show();
+
+  });
+
+
+  //reset search button on mobile (black cross)
+  $(".js-reset-search-icon").click(function() {
+
+    $('#navbar_sm_search_input').val('');
+    document.querySelector('#searchbox .ais-SearchBox-input').value = '';
+    search.helper.setQuery(document.querySelector('#searchbox .ais-SearchBox-input').value).search();
+
+  });
+
+
+  //create a random background color for default thumbnails
   function randomColor(){
 
     var colors = (['#884394', '#3f51b5', '#009688', '#42a346', '#ff9800', '#2d66ba', '#a58f4f', '#129d90', '#428392', '#263b78', '#878273', '#705c20', '#d24040', '#c1944e']);
@@ -24,6 +29,8 @@
 
   }
 
+
+  //Format keywords (separate and add #)
   function formatKeywords(keywordsString){
 
     if(keywordsString != ''){
@@ -33,7 +40,9 @@
     return keywordsString;    
 
   }
-  
+
+
+  //Initialize Algolia instant search with credentials  
   const search = instantsearch({
     indexName: app_env+'_presentation_bases',
     searchClient: algoliasearch(
@@ -41,29 +50,9 @@
       'b2d9ba779ea94f1c5f81d1b5751e267e'
     ),
   });
-   
-  search.on('render', function () {  
 
-    $("#navbar_md_search_input, #navbar_sm_search_input").on("input", function() {
 
-      $(".ais-InstantSearch").show();
-
-      //small screens reset search icon appearance / diappearance
-
-      if (!this.value) {
-        $(".js-reset-search-icon-container").hide();
-      } else {$(".js-reset-search-icon-container").show();}
-
-      document.querySelector('#searchbox .ais-SearchBox-input').value = $(this).val();
-
-      search.helper.setQuery(document.querySelector('#searchbox .ais-SearchBox-input').value).search();
-      
-      $("#main-body-container").hide();
-
-    });
-
-  });
-
+  //#searchbox div is the only search input recipient (.ais-SearchBox-input)
   search.addWidgets([
 
     instantsearch.widgets.searchBox({
@@ -71,7 +60,42 @@
       container: '#searchbox',
 
     }),
-    
+
+  ]);
+
+  search.on('render', function () {//render fires when all widgets are rendered. This happens after every search request.
+
+    //Below are some search input proxies. $('').on("input") event means : "each time a proxy has changed" 
+    $("#navbar_md_search_input, #navbar_sm_search_input, #homepage-search-input").on("input", function() {
+
+      $(".ais-InstantSearch").show(); //display search results panel
+
+      //small screens : "reset search" button  (black cross icon) : appearance only if input is feeded
+      if (!this.value) {
+        $(".js-reset-search-icon-container").hide();
+      } else {$(".js-reset-search-icon-container").show();}
+
+
+      //put the proxy input value into "the only one true search input" (i.e. #searchbox .ais-SearchBox-input input)
+      document.querySelector('#searchbox .ais-SearchBox-input').value = $(this).val();
+      
+      //then refresh algolia search
+      search.helper.setQuery(document.querySelector('#searchbox .ais-SearchBox-input').value).search();
+
+      if($(this).attr("id")=="homepage-search-input"){
+        $("#main-body-container").addClass("visually-hidden")
+
+      }else{$("#main-body-container").hide();} //hide the rest}
+
+
+    });
+
+  });
+
+
+  search.addWidgets([
+
+    //instantiate a search result filters panel (left panel)
     instantsearch.widgets.refinementList({
 
       container: '#filters-list',
@@ -80,60 +104,67 @@
       
     }),
 
-
+    //instantiate a search results panel (right panel)
     instantsearch.widgets.hits({
 
       container: '#hits',
 
-      templates: { /* Twig verbatim is used to avoid curly brackets (braces) conflicts */
-
+      templates: { //search results cards template
         
         item:function(data) {
 
+          //wrapping card with a casual link to appropriate project page
           output = '<a href="'+domain+data.stringId+'" data-id="'+data.id+'" class="hit-link-wrap">';
 
+          //preparing card's thumbnail
           var imgContent;
 
+            //if we have an image path we use it
             if (data.cache && data.cache.thumbnailAddress){
 
               imgContent = '<img class="hit-img" src="'+data.cache.thumbnailAddress+'" align="left" alt="Thumbnail" />';
 
             }
 
-            else {
+            else {//creating a default thumbnail
 
               imgContent= '<div class="avatar-square-rounded avatar-80 mx-auto" style="background-color:'+randomColor()+';"><span class="avatar-initial avatar-initial-80">'+data.goal.charAt(0).toUpperCase()+'</span></div>';
             
             }
 
+          //card's image container + feeding it
           output += '<div class="hit-img-ctn">'+imgContent+'</div>';
 
-          output += '<div class="hit-txt-ctn"><div class="hit-flex-ctn">'
-            
-          output += '<div class="hit-goal">'+data._highlightResult.goal.value+'</div>';
+          //preparing card's text content
 
-          var titleContent = '';
-          if (data.title){titleContent = data._highlightResult.title.value;}
-          output += '<div class="hit-title">'+titleContent+'</div>';
+            var textContent;
 
-          var keywordsContent = '';
-          if (data.keywords){keywordsContent = data._highlightResult.keywords.value;}
-          output += '<div class="hit-keywords">'+formatKeywords(keywordsContent)+'</div>';
+            //presentation goal
+            textContent = '<div class="hit-goal">'+data._highlightResult.goal.value+'</div>';
 
-          output += '</div></div></a>'; //end of text container + link wrapper
+            var titleContent = '';
+            if (data.title){titleContent = data._highlightResult.title.value;}
+            textContent += '<div class="hit-title">'+titleContent+'</div>';
+
+            var keywordsContent = '';
+            if (data.keywords){keywordsContent = data._highlightResult.keywords.value;}
+            textContent += '<div class="hit-keywords">'+formatKeywords(keywordsContent)+'</div>';
+
+          //card's text container + feeding it with text content + end of link wrapper
+          output += '<div class="hit-txt-ctn"><div class="hit-flex-ctn">'+textContent+'</div></div></a>'; 
           
           return output
 
 
         },
 
-
-        empty: '<div>Désolé, aucun résultat n\'a été trouvé pour la recherche "{{ query }}".</div>.'
+        empty: '<div>Désolé, aucun résultat n\'a été trouvé pour la recherche "{{ query }}".</div>'
 
       },
       
     }),
 
+    //instantiate a search results pagination
     instantsearch.widgets.pagination({
       container: '#pagination',
     }),
@@ -144,9 +175,10 @@
 
   ]);
 
-  if(typeof selection_instance !== 'undefined'){ //add a drag and drop capability to search results, see selection_manage.html
+  //only for presentations pick up instances (// see manage_select_presentations.html)
+  if(typeof selection_instance !== 'undefined'){ //add a drag and drop capability to search results (see selection_manage.html).
 
-    search.on('render', function () {  
+    search.on('render', function () {//Whenever instant search is refreshed
       
       $('.ais-Hits-list').sortable({
 
@@ -154,8 +186,8 @@
 
         group: {
 
-          name:'results-to-selection',
-          put: false // Do not allow items to be put into this parent list
+          name:'results-to-selection', // groups which share same names are "drag and drop linked".
+          put: false // Do not allow items to be put into the parent list
 
         },
 
@@ -171,45 +203,42 @@
 
         },
 
-        onRemove: function (evt) {
+        onRemove: function (evt) {//each time a presentation is picked from search results.
 
-            // changing dragged item wrapping tag (otherwise item disappear each time I click on an algolia pagination number)
-            $(evt.item).replaceWith($('<div class="selected-item">').append($(evt.item).contents()).append('<button type"button" class="js-remove-elem">&times</button>'));
+          //changing dragged item wrapping tag (otherwise item disappear each time I click on an algolia pagination number)
+          $(evt.item).replaceWith($('<div class="selected-item">').append($(evt.item).contents()).append('<button type"button" class="js-remove-elem">&times</button>'));
 
-            //$(evt.item).removeClass("ais-Hits-item");
+          // preparing an array to store "picked elements positions" by id
+          var elementsPositions = [];
 
-            // an array storing elements positions by id
+          //getting elements positions              
+          $('.selected-items-panel a').each(function(index){
+                          
+            elementsPositions.push($(this).data('id'));
+        
+          });
 
-            var elementsPositions = [];
-              
-            $('.selected-items-panel a').each(function(index){
-                            
-              elementsPositions.push($(this).data('id'));
-          
-            });
+          $.ajax({  
 
-            $.ajax({  
+            url: pick_up_elements_route, // see manage_select_presentations.html
+            type:       'POST',   
+            dataType:   'json',
+            data: {
+                "selectionType": 'headlines',
+                "jsonElementsPosition": elementsPositions,
+            },
 
-              url: pick_up_elements_route, // see manage_select_presentations
-              type:       'POST',   
-              dataType:   'json',
-              data: {
-                  "selectionType": 'headlines',
-                  "jsonElementsPosition": elementsPositions,
-              },
+            async: true,  
+            
+            success: function(data, status) {
+                    
+            },  
 
-              async: true,  
-              
-              success: function(data, status) {
-                      
-              },  
+            error : function(xhr, textStatus, errorThrown) {  
+              alert('Une erreur est survenue.');  
+            }  
 
-              error : function(xhr, textStatus, errorThrown) {  
-                alert('Une erreur est survenue.');  
-              }  
-
-            }); 
-
+          }); 
 
         },
 
@@ -220,8 +249,6 @@
   }
 
   search.start();
-
-  
 
 });
 

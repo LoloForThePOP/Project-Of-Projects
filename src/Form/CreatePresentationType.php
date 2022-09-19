0eb853c9d3a27ad7bd6d431b2b8124ab7c6a1548
LoloForThePop <lauguy@free.fr>
2022-09-19 18:21:21 +0200

    Homepage design WIP

--- src/Form/CreatePresentationType.php
+++ src/Form/CreatePresentationType.php
@@ -21,7 +21,7 @@ class CreatePresentationType extends AbstractType
                 TextType::class,
 
                 [
-                    'label' => 'Objectif du Projet',
+                    'label' => "Quel est l'objectif du Projet ?",
 
                     'attr' => [
 

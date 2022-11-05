dd553ebfae173807ec1f9a233b40785640de4e35
LoloForThePop <lauguy@free.fr>
2022-11-05 16:08:07 +0100

    Tried to improve page load (after image Zoom implementation on large screens)

--- public/js/image_zoom.js
+++ public/js/image_zoom.js
@@ -1,98 +1,127 @@
+	// thanks at https://www.w3schools.com/howto/howto_js_image_zoom.asp
+  function imageZoom(imgParentWrapperId, resultID) {
 
-// thanks at https://www.w3schools.com/howto/howto_js_image_zoom.asp
+    var img, lens, result, cx, cy, timeout, imgWrapper,
 
+    imgWrapper = document.getElementById(imgParentWrapperId);
 
-function imageZoom(imgParentWrapperId, resultID) {
+    result = document.getElementById(resultID);
 
-  var img, lens, result, cx, cy;
-  img = document.getElementById(imgParentWrapperId).getElementsByTagName('img')[0];
-
-  console.log(img);
-  result = document.getElementById(resultID);
-
-  console.log(result);
-
-  /* Create lens: */
-  lens = document.createElement("DIV");
-  lens.setAttribute("class", "img-zoom-lens");
-
-  /* Insert lens: */
-  img.parentElement.insertBefore(lens, img);
-
-  console.log("resOffWidth"+result.offsetWidth);
-  console.log("resOffHeight"+result.offsetHeigth);
-  console.log("lensOffWidth"+lens.offsetWidth);
-  console.log("lensOffHeight"+lens.offsetHeight);
-
-  /* Calculate the ratio between result DIV and lens: */
-  cx = result.offsetWidth / lens.offsetWidth;
-  cy = result.offsetHeight / lens.offsetHeight;
-
-  /* Set background properties for the result DIV */
-  result.style.backgroundImage = "url('" + img.src + "')";
-  result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
-
-  /* Execute a function when someone moves the cursor over the image, or the lens: */
-  lens.addEventListener("mousemove", moveLens);
-  img.addEventListener("mousemove", moveLens);
-
-  /* And also for touch screens: */
-  lens.addEventListener("touchmove", moveLens);
-  img.addEventListener("touchmove", moveLens);
-
-
-  function moveLens(e) {
-    var pos, x, y;
-
-    /* Prevent any other actions that may occur when moving over the image */
-    e.preventDefault();
-
-    /* Get the cursor's x and y positions: */
-    pos = getCursorPos(e);
-
-    /* Calculate the position of the lens: */
-    x = pos.x - (lens.offsetWidth / 2);
-    y = pos.y - (lens.offsetHeight / 2);
+    if (imgWrapper==null){return;} //case slide do not contains a image (for example it contains a video)
     
-    /* Prevent the lens from being positioned outside the image: */
-    if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
-    if (x < 0) {x = 0;}
-    if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
-    if (y < 0) {y = 0;}
+    else{
 
-    /* Set the position of the lens: */
-    lens.style.left = x + "px";
-    lens.style.top = y + "px";
+      img = imgWrapper.getElementsByTagName('img')[0];
+
+      /* Create lens: */
+      lens = document.createElement("DIV");
+      lens.setAttribute("class", "img-zoom-lens");
+
+      /* Insert lens: */
+      img.parentElement.insertBefore(lens, img);
+
+      /* Calculate the ratio between result DIV and lens: */
+      cx = result.offsetWidth / lens.offsetWidth;
+      cy = result.offsetHeight / lens.offsetHeight;
+
+      /* Set background properties for the result DIV */
+      result.style.backgroundImage = "url('" + img.src + "')";
+      result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
+
+
+
+      /* Execute a function when someone moves the cursor over the image, or the lens: */
+      lens.addEventListener("mousemove", moveLens);
+      img.addEventListener("mousemove", moveLens);
+      
+      lens.addEventListener("mouseout", closeZoom);
+      img.addEventListener("mouseout", closeZoom);
+
+      /* And also for touch screens:
+      lens.addEventListener("touchmove", moveLens);
+      img.addEventListener("touchmove", moveLens); */
+
+
+      lens.style.opacity="0";
 
     
+    }
 
-    /* Display what the lens "sees": */
-    result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
 
-    console.log("x"+x);
-    console.log("cx"+cx);
-    console.log("y"+y);
-    console.log("cy"+cy);
+    function moveLens(e) {
+
+      var pos, x, y;
+
+      /* Prevent any other actions that may occur when moving over the image */
+      e.preventDefault();
+
+      /* Make lens appears */
+      lens.style.opacity="0.2";
+
+      /* Get the cursor's x and y positions: */
+      pos = getCursorPos(e);
+
+      /* Calculate the position of the lens: */
+      x = pos.x - (lens.offsetWidth / 2);
+      y = pos.y - (lens.offsetHeight / 2);
+      
+      /* Prevent the lens from being positioned outside the image: */
+      if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth; }
+      if (x < 0) {x = 0;}
+      if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
+      if (y < 0) {y = 0;}
+
+      /* Set the position of the lens: */
+      lens.style.left = x + "px";
+      lens.style.top = y + "px";
+
+      /* Display what the lens "sees": */
+      result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
+
+
+      result.style.marginLeft = "auto";
+
+      
+      
+    }
+
+    function closeZoom(e) {
+
+      result.style.marginLeft = "-10000px";
+      
+    }
+
+    function getCursorPos(e) {
+
+
+      var a, x = 0, y = 0;
+      e = e || window.event;
+
+      /* Get the x and y positions of the image: */
+      a = img.getBoundingClientRect();
+
+      /* Calculate the cursor's x and y coordinates, relative to the image: */
+      x = e.pageX - a.left;
+      y = e.pageY - a.top;
+
+      /* Consider any page scrolling: */
+      x = x - window.pageXOffset;
+      y = y - window.pageYOffset;
+      return {x : x, y : y};
+
+    }
 
   }
 
-  function getCursorPos(e) {
 
-    var a, x = 0, y = 0;
-    e = e || window.event;
+  function killImageZoom(){
 
-    /* Get the x and y positions of the image: */
-    a = img.getBoundingClientRect();
+    const elements = document.getElementsByClassName("img-zoom-lens");
 
-    /* Calculate the cursor's x and y coordinates, relative to the image: */
-    x = e.pageX - a.left;
-    y = e.pageY - a.top;
+    while(elements.length > 0){
 
-    /* Consider any page scrolling: */
-    x = x - window.pageXOffset;
-    y = y - window.pageYOffset;
-    return {x : x, y : y};
+      elements[0].parentNode.removeChild(elements[0]);
+
+    }
 
   }
-
-} 

32cb48661e13bc2eaeb4a4bda26cbd5ae03c4dbe
LoloForThePop <lauguy@free.fr>
2023-09-18 11:16:16 +0200

    changed some show more button tags to a tags for SEO

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -21,10 +21,10 @@ class SearchController extends AbstractController
      * 
      * label : the label displayed on view (ex: "Environmental Projects").
      * 
-     * @Route("/backend-search/{label}/{filters}/{iconName}/{shuffle}", name="backend_search")
+     * @Route("/backend-search/{label}/{filters}/{shuffle}/{iconName}", name="backend_search")
      *
      */
-    public function backendSearch($label='', $iconName='', $filters='none', $shuffle=0)
+    public function backendSearch($label='', $iconName = null, $filters='none', $shuffle = 0)
     {
 
         $em = $this->getDoctrine()->getManagerForClass(PPBase::class);

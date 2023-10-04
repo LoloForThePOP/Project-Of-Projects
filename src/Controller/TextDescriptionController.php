b3317efbf35920fa6f924d481724ea910a9f99c2
LoloForThePOP <57801012+LoloForThePOP@users.noreply.github.com>
2023-10-04 21:22:04 +0200

    WIP cool project of the day

--- src/Controller/TextDescriptionController.php
+++ src/Controller/TextDescriptionController.php
@@ -11,12 +11,15 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class TextDescriptionController extends AbstractController
 {
+
+    
     /**
      * 
      * @Route("/projects/{stringId}/edit/text-description", name="edit_pp_text_description")
@@ -57,4 +60,59 @@ class TextDescriptionController extends AbstractController
             ]
         );
     }
+
+    /**
+     * 
+     * @Route("/projects/{stringId}/edit/editorial-text-description", name="edit_pp_editorial_text_description")
+     * 
+     */
+    public function editorial(PPBase $presentation, Request $request, EntityManagerInterface $manager)
+    {
+
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        $form = $this->createFormBuilder()
+            ->add('editorial', TextareaType::class,
+            [
+                'required'     => false,
+                'attr' => [
+                    'class' => "tinymce",
+                ],
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $editorial = $form->get('editorial')->getData();
+
+            $presentation->setDataItem('short_editorial_text_fr', $editorial);
+
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "✅ Modification effectuée"
+            );
+
+            return $this->redirectToRoute('show_presentation', [
+                'stringId' => $presentation->getStringId(),
+                '_fragment' => 'text-description-struct-container',
+            ]);
+        }
+
+        return $this->render(
+
+            'project_presentation/edit/text_description/editorial.html.twig',
+            [
+                'form' => $form->createView(),
+                'stringId' => $presentation->getStringId(),
+            ]
+        );
+    }
+
+
+
+
 }

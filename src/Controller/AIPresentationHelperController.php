836dd358bd6561eb554c0846ea53fe6ee5835ef0
LoloForThePop <lauguy@free.fr>
2023-10-21 21:15:31 +0200

    WIP Project Presentation Advice AI Assistant

--- src/Controller/AIPresentationHelperController.php
+++ src/Controller/AIPresentationHelperController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AIPresentationHelperController extends AbstractController
 {
-    #[Route('/ia-coach-gratuit-presentation-de-projet', name: 'ai_presentation_helper')]
+    #[Route('/ia-assistant-gratuit-de-presentation-de-projet', name: 'ai_presentation_helper')]
     public function index(): Response
     {
         return $this->render('ai_presentation_helper/origin.html.twig', [

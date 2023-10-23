b928598c2ec6ed3fbbf306aadfed67782ab15af0
LoloForThePop <lauguy@free.fr>
2023-10-23 10:27:18 +0200

    WIP Project Presentation Advice AI Assistant

--- src/Controller/AIPresentationHelperController.php
+++ src/Controller/AIPresentationHelperController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\AIPPAdviceType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AIPresentationHelperController extends AbstractController
 {
     #[Route('/ia-assistant-gratuit-de-presentation-de-projet', name: 'ai_presentation_helper')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+
+        $form = $this->createForm(AIPPAdviceType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+           //dd("form submitted and valid");
+
+        }
+
         return $this->render('ai_presentation_helper/origin.html.twig', [
-            'controller_name' => 'AIPresentationHelperController',
+            'form' => $form->createView(),
         ]);
+
+
+
+
     }
 }
--- src/Form/AIPPAdviceType.php
+++ src/Form/AIPPAdviceType.php
@@ -5,13 +5,28 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AIPPAdviceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+
+            ->add(
+                'ppTarget', 
+                TextareaType::class,
+                [
+
+                    'label' => 'ppTarget',
+                    'required'   => true,
+                    'attr' => [
+
+                        'placeholder'    => "Écrire ici. Apportez des précisions pour obtenir une meilleure présentation.",
+                    ],
+                ]
+            )
+
         ;
     }
 

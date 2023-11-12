5a2332c179d686d6d7571ba7b80568dd2d51dd9a
LoloForThePop <lauguy@free.fr>
2023-11-12 15:28:45 +0100

    WIP - project news display

--- public/js/show_more_less_button.js
+++ public/js/show_more_less_button.js
@@ -17,7 +17,7 @@ window.onload = function() {
       var el = document.createElement("a");
       el.innerHTML = "Afficher +";
       el.setAttribute("type", "button");
-      el.setAttribute("class", "read-more");
+      el.setAttribute("class", "read-more sm-display-none");
       el.setAttribute("src", "#");
       
       insertAfter(allOSB[i], el);
@@ -50,7 +50,7 @@ function updateReadMore() {
         }
         allOSB[i].nextElementSibling.className = "read-more btn btn-primary btn-sm mt-2";
       } else {
-        allOSB[i].nextElementSibling.className = "read-more";
+        allOSB[i].nextElementSibling.className = "read-more sm-display-none";
       }
     }
   }

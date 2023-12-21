ae91785ca199c5b1743f2d1fdecf20ecf81b75bf
LoloForThePop <lauguy@free.fr>
2023-12-21 19:01:22 +0100

    WIP - ia logo creation helper - backend user inputs treatment - WIP promptisation of user inputs

--- src/Service/IALogoService.php
+++ src/Service/IALogoService.php
@@ -32,7 +32,7 @@ class IALogoService {
 
             dump("treating step $step with value $value");
 
-            if($value !== null || $value !== ""){
+            if($value !== null && $value !== ""){
 
                 $prompt = $prompt.$this->createPromptChunk($step, $value);
 
@@ -47,27 +47,26 @@ class IALogoService {
     }
 
     /**
-    * 
+    * Create a prompt chunk according to the step taken by the user and the value he provided
     */
-
     protected function createPromptChunk($step, $value){
 
-        $Fr_Prompt = "";
+        $Fr_prompt_chunk = "";
         
         switch ($step) {
 
             case 'big_cats':
 
                 if ($value == "letters") {
-                    $Fr_Prompt = "Créer un logo de type une ou plusieurs lettres. ";
+                    $Fr_prompt_chunk = "Créer un logo de type une ou plusieurs lettres. ";
                 }
 
                 elseif ($value == "image") {
-                    $Fr_Prompt = "Créer un logo sans aucune lettre. ";
+                    $Fr_prompt_chunk = "Créer un logo sans aucune lettre. ";
                 }
 
                 else {
-                    $Fr_Prompt = "Créer un logo de type lettres et images. ";
+                    $Fr_prompt_chunk = "Créer un logo de type lettres et images. ";
                 }
                 
                 break;
@@ -75,53 +74,114 @@ class IALogoService {
             case 'logo_type_letters':
                 
                 if ($value == "one-letter") {
-                    $Fr_Prompt = "Ce logo devra contenir UNE SEULE lettre. ";
+                    $Fr_prompt_chunk = "Ce logo devra contenir UNE SEULE lettre. ";
                 }
                 
                 elseif ($value == "several-letters") {
-                    $Fr_Prompt = "";
+                    $Fr_prompt_chunk = "";
                 }
                 
                 elseif ($value == "tangled-letters") {
-                    $Fr_Prompt = "Les lettres devront être enlacées (par exemple comme avec le logo de la marque Yves Saint Laurent, ou le logo de la marque Louis Vuitton. ";
+                    $Fr_prompt_chunk = "Les lettres devront être enlacées (par exemple comme avec le logo de la marque Yves Saint Laurent, ou le logo de la marque Louis Vuitton. ";
                 }
+
+                break;
+
+            case 'one_letter_user_choice':
+                
+                $Fr_prompt_chunk = "Cette lettre est la lettre $value. ";
+
+                break;
+
+            case 'several_letters_user_choice':
+                
+                $Fr_prompt_chunk = "Les lettres à insérer dans le logo, en respectant l'ordre, les majuscules, et les éventuels espaces, sont les suivantes : $value. ";
+
+                break;
+
+            case 'tangled_letters_user_choice':
+                
+                $Fr_prompt_chunk = "Les lettres à insérer et enlacer entre elles dans le logo, en respectant l'ordre, les majuscules, et les éventuels espaces, sont les suivantes : $value. ";
+
+                break;
+
+            case 'font_type':
+                
+                $Fr_prompt_chunk = "Voici la police de caractère à utiliser : $value. ";
+
+                break;
+
+            case 'letter_colors':
+                
+                $Fr_prompt_chunk = "Voici les instructions pour la couleur des lettres : $value. ";
 
                 break;
 
             case 'logo_relative_positions':
                 
                 if ($value == "text-left") {
-                    $Fr_Prompt = "Le texte est positionné à gauche de l'image. ";
+                    $Fr_prompt_chunk = "Le texte est positionné à gauche de l'image. ";
                 }
                 
                 elseif ($value == "text-right") {
-                    $Fr_Prompt = "Le texte est positionné à droite de l'image. ";
+                    $Fr_prompt_chunk = "Le texte est positionné à droite de l'image. ";
                 }
                 
                 elseif ($value == "text-bottom") {
-                    $Fr_Prompt = "Le texte est positionné sous l'image. ";
+                    $Fr_prompt_chunk = "Le texte est positionné sous l'image. ";
                 }
                 
                 elseif ($value == "text-top") {
-                    $Fr_Prompt = "Le texte est positionné au dessus de l'image. ";
+                    $Fr_prompt_chunk = "Le texte est positionné au dessus de l'image. ";
                 }
                 
                 elseif ($value == "text-through") {
-                    $Fr_Prompt = "Le texte est positionné à l'intérieur de l'image. ";
+                    $Fr_prompt_chunk = "Le texte est positionné à l'intérieur de l'image. ";
                 }
                 
                 elseif ($value == "text-circle") {
-                    $Fr_Prompt = "Le texte est positionné circulairement autour de l'image (text circle). ";
+                    $Fr_prompt_chunk = "Le texte est positionné circulairement autour de l'image (text circle). ";
                 }
 
                 break;
+
+        
+            case 'image_type':
+
+                if ($value == "figurative-objects") {
+                    $Fr_prompt_chunk = "L'image représente un objet de la réalité. ";
+                }
+                
+                elseif ($value == "abstraction") {
+                    $Fr_prompt_chunk = "L'image ne représente pas un objet de la réalité, c'est un objet abstrait'. ";
+                }
+                
+                else {
+                    $Fr_prompt_chunk = "Le type d'image n'est pas connu. ";
+                }
+
+                break;
+        
+            case 'abstraction_object_description':
+                
+                $Fr_prompt_chunk = "Voici la description de l'objet abstrait à insérer dans le logo : $value ";
+
+                break;
+    
+        
+            case 'figurative_object_description':
+                
+                $Fr_prompt_chunk = "Voici la description de l'objet de la réalité à insérer dans le logo : $value ";
+
+                break;
+    
             
             default:
                 throw new \Exception("Unsupported step type");
                 break;
        }
 
-       return $Fr_Prompt;
+       return $Fr_prompt_chunk;
 
     }
 

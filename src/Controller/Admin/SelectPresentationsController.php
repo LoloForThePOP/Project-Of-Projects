3202a81f6dd896efc303e73e7e3d2445e4744a52
LoloForThePop <lauguy@free.fr>
2024-09-26 12:09:23 +0200

    WIP - Clean Propon Code - WIP Homepage

--- src/Controller/Admin/SelectPresentationsController.php
+++ src/Controller/Admin/SelectPresentationsController.php
@@ -8,16 +8,22 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
+/**
+ * This controller gathers functions that select and render some specific project lists
+ * Ex: admin selects some specific project presentations he wants to highlight on homepage.
+ */
+
 class SelectPresentationsController extends AbstractController
 {
+    // Paths of some specific project list stored in json format
 
     protected $genericStoragePath = '../templates/select_presentations/editor_selection.json';
     protected $projectOfTheDay = '../templates/select_presentations/project_of_the_day.json';
     
    
     /**
-     * Allow to manage presentation headlines (i.e. editor's pick for homepage).
-     * Pick up some presentations and store them in the website headline.
+     * Allow an admin to pick up some project presentations; store a list of these projects; manage this list (so that we can reuse that project list later on, for example we highlight a list of projects on homepage)
      * 
      * @Route("/admin/manage-pick-elements/{pickType}", name="manage_pick_elements")
      *
@@ -25,14 +31,17 @@ class SelectPresentationsController extends AbstractController
     public function managePickElements(PPBaseRepository $ppRepo, string $pickType)
     {
 
-
         $storagePath = null;
 
+        // The path where we get the project list according to the selection criteria
+
         switch ($pickType) {
-            case 'trustUs':
+
+            case 'trustUs': // "They trust us" Projects (social aprouval on homepage)
                 $storagePath = $this->genericStoragePath;
                 break;
-            case 'projectOfTheDay':
+
+            case 'projectOfTheDay': // when we pick up one project we want to highlight on homepage
                 $storagePath = $this->projectOfTheDay;
                 break;
             
@@ -42,6 +51,8 @@ class SelectPresentationsController extends AbstractController
         }
 
         $currentSelectionItems = [];
+
+        // Getting in database the current specific project list so that admin can see it and update it
 
         if ($currentSelectionIds = json_decode(file_get_contents($storagePath))) {
 
@@ -53,6 +64,8 @@ class SelectPresentationsController extends AbstractController
             
         }
 
+        // Render an admin user interface so that admin can update the considered list
+
         return $this->render('select_presentations/manage.html.twig', [
             'currentSelection' => $currentSelectionItems,
             'pickType' => $pickType,
@@ -62,7 +75,7 @@ class SelectPresentationsController extends AbstractController
 
     /** 
      * 
-     * Ajax request to store editor's selection
+     * Ajax request so that admin can update a specific project list
      *  
      * @Route("/admin/ajax-pick-up-elements/{pickType}", name="manage_pick_up_elements") 
      * 
@@ -74,10 +87,14 @@ class SelectPresentationsController extends AbstractController
 
             $storagePath = null;
 
+            // Getting the appropriate project list path
+
             switch ($pickType) {
+
                 case 'trustUs':
                     $storagePath = $this->genericStoragePath;
                     break;
+
                 case 'projectOfTheDay':
                     $storagePath = $this->projectOfTheDay;
                     break;
@@ -87,10 +104,11 @@ class SelectPresentationsController extends AbstractController
                     break;
             }
 
+            // Updating the json file representing the project list so that we can access it and potentially update it.
+
             $jsonSelection = $request->request->get('jsonElementsPosition');
 
             file_put_contents($storagePath, json_encode($jsonSelection));
-           
 
             return  new JsonResponse(true);
 
@@ -104,21 +122,28 @@ class SelectPresentationsController extends AbstractController
 
     /** 
      * 
-     * Allow to get picked elements (return an hydrated collection of selected elements)
+     * Allow to get a specific project list and render it in directly displayable html format.
+     * This route can be directly called from an twig template so that we directly get and display the list of projects we want.
+     * 
+     * picktype: (string) the name of list you want to render
+     * label: (string) a title you want to give to the project list, users will see it displayed
+     * iconname: (string: name of an icon without its file format) an icon you might want to add before the list title
      *  
      * @Route("/get-picked-elements/{pickType}/{label}/{iconName}", name="get_picked_elements") 
      * 
      */
     public function getPickedElements(PPBaseRepository $ppRepo, string $pickType, $label='none', $iconName = '')
     {
-        $elements = [];
+        $elements = []; // symfony collection of project list
 
-        $storagePath = null;
+        $storagePath = null; // specific storage page to get a specific project list stored in json format
 
         switch ($pickType) {
+
             case 'trustUs':
                 $storagePath = $this->genericStoragePath;
                 break;
+
             case 'projectOfTheDay':
                 $storagePath = $this->projectOfTheDay;
                 break;
@@ -128,9 +153,11 @@ class SelectPresentationsController extends AbstractController
                 break;
         }
 
+        // Getting specific project list in database
+
         if($ids = json_decode(file_get_contents($storagePath))){
 
-            foreach ($ids as $id) { //one by one request to maintain ordering
+            foreach ($ids as $id) { //one by one database request to maintain ordering
 
                 $elements[] = $ppRepo->findOneById($id);
 
@@ -138,8 +165,12 @@ class SelectPresentationsController extends AbstractController
 
         }
 
-          switch ($pickType) {
+        // Calling specific twig template to display specific project list
+
+        switch ($pickType) {
+
             case 'trustUs':
+
                 return $this->render('utilities/_display_collection_wrapper_template.html.twig', [
                     'label' => $label,
                     'iconName' => $iconName,
@@ -158,12 +189,9 @@ class SelectPresentationsController extends AbstractController
             default:
                 throw new \Exception("Unknow manage presentation selections storage path");
                 break;
-        }
 
-       
+        }
         
     }
-
-
 
 }

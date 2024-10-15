a80ca2ef2cdb024ca7c2a9a958db0929659b098f
LoloForThePop <lauguy@free.fr>
2024-10-15 16:09:53 +0200

    WIP - Cleanup & Comment Propon Code - WIP MiscController: sitemap & articles image uploads + sitempap template

--- src/Controller/MiscController.php
+++ src/Controller/MiscController.php
@@ -2,29 +2,24 @@
 
 namespace App\Controller;
 
-use OpenAI;
+
 use App\Entity\User;
-use App\Entity\PPBase;
-use App\Entity\Category;
-use App\Service\OpenAIAPI;
-use App\Repository\UserRepository;
+use App\Service\ImageService;
 use App\Service\SessionVariablesService;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Serializer\Encoder\JsonDecode;
+
 
 class MiscController extends AbstractController
 {
      
      /**
      * 
-     * Render a chunk of an html page, thanks to an ajax call, in order to reduce page load.
+     * Render a chunk of an html page thanks to an ajax call in order to reduce page load.
      * 
      * @Route("/ajax-render-chunk", name="ajax_render_chunk")
      */
@@ -78,8 +73,6 @@ class MiscController extends AbstractController
 
             ];
 
-            //dump($htmlChunk);
-
             return new JsonResponse($htmlChunk);
 
         }
@@ -92,15 +85,12 @@ class MiscController extends AbstractController
     
     /**
      * 
-     * Test something
+     * This route allows to do some tests
      * 
      * @Route("/test-something", name="test_something")
      */
      public function test()
     {
-
-        //$ia = new OpenAIService ($_ENV['OPEN_AI_KEY']);
-        //$answer = $ia->answer("I'm happy but...");
 
         /* UluleAPI $ulule,
         $ulule->fetchProjectInfo(); just testing Ulule api*/
@@ -114,34 +104,34 @@ class MiscController extends AbstractController
     
 
      /**
+      *
+      * Allows to dynamically create a sitemap.xml file when sitemap route is called
+      * 
+      *
      * @Route("/sitemap.xml", name="sitemap", defaults={"_format"="xml"})
      */
     public function sitemap(Request $request, EntityManagerInterface $manager)
     {
-        // Getting hostname
-        $hostname = $request->getSchemeAndHttpHost();
         
-        $sitemapPreparation = [];    // array with potential 'loc', 'title', 'updatedAt'... contents.
+        $sitemapPreparation = []; // Structured array that will be converted to xml format, with potential 'loc', 'title', 'updatedAt'...
             
-        // Static urls :
+        // Static urls management:
 
-        $staticUrls=[];
+        $staticUrls=[]; //array that contains only urls from static pages
 
-        //From static controller :
+        // Static page file names
 
-        $staticControllerPageNames = ["terms", "short_manifesto", "about_us", "credits", "privacy"];
+        $staticPageNames = ["terms", "short_manifesto", "about_us", "credits", "privacy"];
 
+        // Static page urls generation
 
-        //static pages urls generation
+        foreach ($staticPageNames as $value){
 
-        foreach ($staticControllerPageNames as $value){
-
-            $staticUrls[] = $this->generateUrl('static', ['page_name' => $value]);
+            $staticUrls[] = $this->generateUrl('static', ['page_name' => $value]); //generated from staticController
 
         }
 
-
-        //Other static pages urls generation : 
+        // Other static pages urls generation not generated from staticController 
 
         $staticUrls[]=$this->generateUrl('homepage');
 
@@ -153,7 +143,7 @@ class MiscController extends AbstractController
 
         $staticUrls[]=$this->generateUrl('ai_interview_helper_origin');
 
-        // sitemap attributes for static pages
+        // Injecting static pages urls into the sitemap preparation array
 
         foreach ($staticUrls as $value){
 
@@ -161,9 +151,13 @@ class MiscController extends AbstractController
 
         }
 
-        // Project presentations:
+        // Adding project presentations to the sitemap
+
+        // Getting public Project Presentation from DB
 
         $accessiblePresentations = $manager->createQuery('SELECT p FROM App\Entity\PPBase p WHERE p.isPublished=true AND p.overallQualityAssessment>=2 AND p.isAdminValidated=true ORDER BY p.createdAt DESC')->getResult();
+
+        // Formatting these project presentation urls into the sitemap preparation array
 
         foreach ($accessiblePresentations as $accessiblePresentation) {
 
@@ -175,15 +169,17 @@ class MiscController extends AbstractController
 
                 ]),
 
-
             ];
-
 
         }
 
-        // Articles:
+        // Adding articles to the sitemap
+
+        // Getting public articles from DB
 
         $accessibleArticles = $manager->createQuery('SELECT a FROM App\Entity\Article a WHERE a.isValidated=true ORDER BY a.createdAt DESC')->getResult();
+
+        // Fromatting these article urls into the sitemap preparation array
 
         foreach ($accessibleArticles as $accessibleArticle) {
 
@@ -199,13 +195,14 @@ class MiscController extends AbstractController
 
         }
 
+        // Preparing & returning response
         $response = new Response(
-            
-            $this->renderView('sitemap.xml.twig', [
-                'sitemapPreparation' => $sitemapPreparation,
-                'hostname' => $hostname
                 
-            ]
+                $this->renderView('sitemap.xml.twig', [
+                    'sitemapPreparation' => $sitemapPreparation,
+                    'hostname' => $request->getSchemeAndHttpHost()
+                    
+                ]
             )
         );
 
@@ -217,12 +214,15 @@ class MiscController extends AbstractController
 
 
     /**
+     * 
+     * Allows user to upload images from tinyMCE rich text editor (ex: user wants to upload images for an article they are creating).
+     * 
      * @Route("/upload-image", name="upload_image", methods={"POST"})
     */
-    public function uploadImage(Request $request)
+    public function uploadImage(Request $request, ImageService $imageService)
     {
 
-        $session = $request->getSession();
+        $session = $request->getSession(); //to store & check if user has already downloaded a lot of images during their session.
 
         $imagesCount = $session->get('imagesCount'); // we don't want user to upload too much images in one session
 
@@ -231,13 +231,18 @@ class MiscController extends AbstractController
             return $response;
         }
 
+
         $baseUrl = $request->getScheme() . '://' . $request->getHttpHost() . $request->getBasePath();
 
+        // Check if the 'HTTP_ORIGIN' header is present in the request, which is used by browsers
+        // to indicate the origin of the request in Cross-Origin Resource Sharing (CORS) scenarios.
         if (isset($_SERVER['HTTP_ORIGIN'])) {
             // same-origin requests won't set an origin. If the origin is set, it must be valid.
             if ($_SERVER['HTTP_ORIGIN'] == $baseUrl) {
                 header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
-            } else {
+            } else {// If the origin doesn't match the base URL, it means the request is coming
+                // from an untrusted domain. We deny access by returning a JSON response
+                // with a message "Origin Denied" and an HTTP 403 status (Forbidden).
                 $response = new JsonResponse(['message' => 'Origin Denied'], 403);
                 return $response;
             }
@@ -246,10 +251,10 @@ class MiscController extends AbstractController
         // Don't attempt to process the upload on an OPTIONS request
         if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 
-            $response = new JsonResponse(['message' => 'Access-Control-Allow-Methods: POST, OPTIONS']);
+            $response = new JsonResponse(['message' => 'Access-Control-Allow-Methods: POST, OPTIONS']); 
+
             return $response;
         }
-
 
         $file = $request->files->get('file');
 
@@ -257,14 +262,14 @@ class MiscController extends AbstractController
 
         if ($file) {
 
-            //Check file name : 
+            //Checking file name : 
             $filename = $file->getClientOriginalName();
             if (preg_match("/([^\w\s\d\~,;:\[\]\(\).À-ÿ6-8\-_])|([\.]{2,})/", $filename)) {
                 $response = new JsonResponse(["message" => "Le nom du fichier comporte des caracteres non autorises."], 400);
                 return $response;
             }
 
-            // Check extension
+            // Checking file extension
             $extension = $file->getClientOriginalExtension();
 
             $allowedExtensions = ["gif", "jpg", "jpeg", "png", "webp"];
@@ -275,30 +280,30 @@ class MiscController extends AbstractController
                 return $response;
             }
 
-            // Check image size :
-
+            // Checking image size
             $fileSizeInBytes = $file->getSize();
             $maxSizeInBytes = 1024 * 1024 * 7; // Max size 7 Mb.
 
-            if ($fileSizeInBytes > $maxSizeInBytes) {
+            if ($fileSizeInBytes > $maxSizeInBytes) {// case image weight is too large
 
                 $response = new JsonResponse(['message' => 'Le poids de l\'image est trop élevé.'], 400);
 
             }
 
-
-            // Generate a unique file name
+            // Generating a unique file name with extension
             $fileName = md5(uniqid()) . '.' . $file->guessExtension();
 
-            // Move file to appropriate upload folder
+            // Moving image file to appropriate upload folder
             $file->move(
                 $this->getParameter('app.image_upload_directory'),
                 $fileName
             );
 
-            $session->set('imagesCount', $imagesCount++); // we increase images count to further check if max count is not exceeded
+            //CONDITIONAL TO DO: reduce image size using an image resizer service (we got one called ImageResizer). I have not done it now because as of 2024 - 10 - 15 just a few user have uploaded images for articles versus it could take a bit of coding time as for now the ImageResizer service only handles images that are attributes of entities (which is not the cas here, article images are not associated with article entities, they just have a specific path). Note that technically we could inject these articles images without entities into actual article Entities using (https://github.com/dustin10/VichUploaderBundle/blob/master/docs/other_usages/replacing_file.md). Would worth it if this route is used enough :)
 
-            // Return a json response with file location
+            $session->set('imagesCount', $imagesCount++); // Increasing images count in a session to check if next time user upload an image the max count is exceeded or not
+
+            // Returning a json response with server side file location
             return new Response(json_encode(['location' => $baseUrl."/".$this->getParameter('app.image_upload_directory'). $fileName]));
         }
 
@@ -309,18 +314,16 @@ class MiscController extends AbstractController
 
 
     /**
-     * 
-     * 
-     * 
+     * When a user authenticates to the app, we check if this user has just been an anymous user presenting a project as a guest. If true, we get the matching db guest user id stored in a session variable so that we give back to the authenticating user the work he / she has done as a guest.
      * 
      * @Route("/auth-redirections", name="auth_redirections")
     */
     public function authRedirections(SessionVariablesService $sessionVariables, EntityManagerInterface $em)
     {
 
-        $guestUserId = $sessionVariables->guestUserId(); //getting the matching db guest user id stored in a session variable so that we can match anonymous online user that is logging into the app with the work stored in db as a guest user.
+        $guestUserId = $sessionVariables->guestUserId(); //checking if authenticating user has just been a guest user anonymously presenting a project (true if not null).
 
-        if ($guestUserId !== null) {//if user authenticating have a guest user id stored in a session variable, it means that he / she just used the app as a guest and now he authenticates (register or log in).
+        if ($guestUserId !== null) {//if authenticating user have a guest user id stored in a session variable, it means that they just used the app as a guest and now he authenticates to the app.
 
             $guestUser = $this->getDoctrine()->getRepository(User::class)->findOneBy(['id' => $guestUserId]);//searching in db the guest user matching with the online anonymous user that is logging into the app.
 
@@ -344,10 +347,5 @@ class MiscController extends AbstractController
         return $this->redirectToRoute('homepage');
 
     }
-
-
-
-
-
 
 }
--- src/Service/ImageResizer.php
+++ src/Service/ImageResizer.php
@@ -8,9 +8,16 @@ use App\Entity\Persorg;
 use \Gumlet\ImageResize;
 use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
 
+
+/**
+ * Allows to resize images in order to reduce storage.
+ * 
+ * Uses \Gumlet\ImageResize to do resize treatments (https://github.com/gumlet/php-image-resize) (PHP library to resize, scale and crop images).
+ * 
+ */
 class ImageResizer {
 
-    protected $uploaderHelper;
+    protected $uploaderHelper; //Allows to retrieve the path of an image given the entity name containing this image (ex: if an image is an attribute of a slide entity, the uploader helper will give us the path of this image) (note: we must precise the attribute name as a second argument if the entity contains several image attributes) (from Vich\UploaderBundle, a symfony bundle that manage file uploads).
 
     public function __construct(UploaderHelper $uploaderHelper)
     {
@@ -21,48 +28,56 @@ class ImageResizer {
 
 
     /**
-    * Allow to modify an image according to its category (for example : project logo; teammate picture; presentation slide; ...)
+    * 
+    * Allow to resize an image according to its category (for example : project logo; teammate picture; presentation slide; ...)
+    *
+    * Params: 
+    *
+    *   - $imageEntity: the entity containing the image attribute (ex: a Slide entity can contain an image describing a project (this entity can also contain a caption and some other attributes)).
+    *   - $fieldName: some entities contains several image attributes (ex: a News entity can contain several images). In this case to proceed to a specific image we must remove ambiguity so we precise the fieldName (attribute name) of the entity involved (ex: entity: News; fieldName: image2 means I want to proceed on image 2 from a News entity)
+    *
     */
-
-    public function edit ($imageEntity, ?string $fieldName = null){
+    public function edit($imageEntity, ?string $fieldName = null){
        
-        $imagePath = substr($this->uploaderHelper->asset($imageEntity, $fieldName), 1);
+        $imagePath = substr($this->uploaderHelper->asset($imageEntity, $fieldName), 1);//getting image path to then treat this image
         
-        if ($imagePath != false) {
+        if ($imagePath != false) { // If an image path is retrieved
+
+            //getting file extension (treatments depends on file extension)
 
             $imageExt = strtolower(substr($imagePath, strrpos($imagePath, '.') + 1));
 
-            //file extension below are not resized due to gumlet limitations
 
-            if ($imageExt == 'svg') {
+            if ($imageExt == 'svg') {// Due to Gumlet limitations we can't proceed to reduce svg file weights.
 
                 return false;
                 
             }
 
-            $image = new ImageResize($imagePath);
-            $image->quality_jpg = 100;
-            $image->quality_webp = 6;
-            $image->quality_png = 6;
+            $image = new ImageResize($imagePath); // Running a Gumlet library instance
+            
+            //Setting some image quality compression we apply depending on file extension
 
-            if ($imageEntity instanceof Slide) {
-                $image->resizeToBestFit(900, 900);
+            $image->quality_jpg = 100; // from 0 (worst quality, smaller file) to 100 (best quality, biggest file)
+            $image->quality_webp = 6; // ??? to be checked ??? ranges from 0 (worst quality, smaller file) to 100 (best quality, biggest file). 
+            $image->quality_png = 6; //from (no compression) to 9 (highest compression, worst quality).
+
+            if ($imageEntity instanceof Slide) {// If image is an attribute of a Slide entity
+
+                $image->resizeToBestFit(900, 900);//image is resized to fit in a 900 * 900 square without other alteration
             }
 
-            //PPBase instance means image is a project logo, or thumbnail, 
-            //persorg instance means image is a persorg or organisation avatar
-            //so in any case, we reduce image width
-            elseif ($imageEntity instanceof PPBase or $imageEntity instanceof Persorg){
+            //
+            elseif ($imageEntity instanceof PPBase or $imageEntity instanceof Persorg){// If image is an attribute of a PPBase entity (ex: image is a project logo; a project presentation thumbnail) or if image is an attribute of a Persorg entity (ex: image is a person avatar)
 
-                $image->resizeToBestFit(340, 340);
+                $image->resizeToBestFit(340, 340); // image is resized to fit in a 340 * 340 square without other alteration
                 
             }
 
-            $image->save($imagePath);
+            $image->save($imagePath); //saving image changes
 
         }
 
     }
-
 
 }
